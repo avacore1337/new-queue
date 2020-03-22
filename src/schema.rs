3372697122510dec6db26f6b6a7af71d840eeb78
Anton Bäckström <antonbcm@gmail.com>
2020-03-22 20:45:12 +0100

    Adding schema.rs

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,38 @@
 table! {
+    kthuser (id) {
+        id -> Int4,
+        username -> Varchar,
+        ugkthid -> Varchar,
+        realname -> Varchar,
+        location -> Varchar,
+        starttime -> Date,
+        gettinghelp -> Bool,
+        helper -> Varchar,
+        help -> Bool,
+        badlocation -> Bool,
+    }
+}
+
+table! {
+    posts (id) {
+        id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+table! {
+    queue (id) {
+        id -> Int4,
+        locked -> Bool,
+        hiding -> Bool,
+        motd -> Varchar,
+        info -> Varchar,
+    }
+}
+
+table! {
     queues (id) {
         id -> Int4,
         name -> Varchar,
@@ -19,6 +53,9 @@ table! {
 }
 
 allow_tables_to_appear_in_same_query!(
+    kthuser,
+    posts,
+    queue,
     queues,
     users,
 );
