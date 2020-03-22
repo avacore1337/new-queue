436367114dda821a6cf813f9bd9b7f27baff7153
Anton Bäckström <antonbcm@gmail.com>
2020-03-22 21:03:45 +0100

    Added schema again

--- src/schema.rs
+++ src/schema.rs
@@ -1,38 +1,4 @@
 table! {
-    kthuser (id) {
-        id -> Int4,
-        username -> Varchar,
-        ugkthid -> Varchar,
-        realname -> Varchar,
-        location -> Varchar,
-        starttime -> Date,
-        gettinghelp -> Bool,
-        helper -> Varchar,
-        help -> Bool,
-        badlocation -> Bool,
-    }
-}
-
-table! {
-    posts (id) {
-        id -> Int4,
-        title -> Varchar,
-        body -> Text,
-        published -> Bool,
-    }
-}
-
-table! {
-    queue (id) {
-        id -> Int4,
-        locked -> Bool,
-        hiding -> Bool,
-        motd -> Varchar,
-        info -> Varchar,
-    }
-}
-
-table! {
     queues (id) {
         id -> Int4,
         name -> Varchar,
@@ -53,9 +19,6 @@ table! {
 }
 
 allow_tables_to_appear_in_same_query!(
-    kthuser,
-    posts,
-    queue,
     queues,
     users,
 );
