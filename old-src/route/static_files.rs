5396997c28756190b5fcbeac22849346f7a8e006
Robert Welin-Berger <avacore1337@gmail.com>
2020-04-03 23:33:59 +0200

    fixing database field names

--- old-src/route/static_files.rs
+++
@@ -1,8 +0,0 @@
-use std::path::{Path, PathBuf};
-use rocket::response::NamedFile;
-
-#[get("/public/public/<file..>")]
-pub fn file(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
-}
-
--- src/models/queue_entry.rs
+++ src/models/queue_entry.rs
@@ -1,9 +1,13 @@
 #![allow(proc_macro_derive_resolution_fallback)]
+use crate::models::user::User;
+use crate::schema::queue_entries;
 use serde::Serialize;
 
 use chrono::{DateTime, Utc};
 
 #[derive(Queryable, Serialize, Associations)]
+#[belongs_to(parent = "User")]
+#[table_name = "queue_entries"]
 pub struct QueueEntry {
     pub id: i32,
     pub user_id: i32,
