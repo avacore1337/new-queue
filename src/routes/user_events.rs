b4596fd623384afb7d87057b103f68c66f866ef1
Robert Welin-Berger <avacore1337@gmail.com>
2020-04-15 19:33:28 +0200

    oups

--- src/routes/user_events.rs
+++ src/routes/user_events.rs
@@ -3,7 +3,7 @@ use chrono::Utc;
 
 use rocket_contrib::json::JsonValue;
 
-#[get("/queues/<queue_name>/queue_entries")]
+#[get("/queues/<queue_name>/user_events")]
 pub fn get_user_events(queue_name: String, conn: db::DbConn) -> Option<JsonValue> {
     Some(json!(db::user_events::for_queue(
         &conn,
