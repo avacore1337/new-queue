8a45f1e91886e60ebdc3f04acc7a24866d87b318
Anton Bäckström <antonbcm@gmail.com>
2020-04-28 21:10:20 +0200

    Fixed config again

--- public/src/configuration.ts
+++ public/src/configuration.ts
@@ -1,2 +1,2 @@
 export const WEB_SOCKET_SERVER_URL = `ws://${window.location.hostname}:7777/ws`;
-export const HTTP_SERVER_URL = `http://${window.location.hostname}${window.location.port === '3000' ? ':8000' : ''}`;
+export const HTTP_SERVER_URL = `http://${window.location.hostname}:${window.location.port === '3000' ? '8000' : window.location.port}`;
