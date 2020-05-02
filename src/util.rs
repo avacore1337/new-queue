c9bff1940a20dcfe0207ed2217cfab881cffb5bb
Robert Welin-Berger <avacore1337@gmail.com>
2020-05-02 17:28:51 +0200

    better version of ldap query

--- src/util.rs
+++ src/util.rs
@@ -11,6 +11,10 @@ use rocket::request::Form;
 use std::thread;
 use std::time::Duration;
 
+const LDAP_USERNAME: &str = "uid";
+const LDAP_REALNAME: &str = "cn";
+const LDAP_UGKTHID: &str = "ugKthid";
+
 pub fn start_scheduled_tasks() {
     thread::Builder::new()
         .name("chrono thread".into())
@@ -48,32 +52,36 @@ pub struct Ticket {
 
 pub fn convert_to_ldap_user(mut rs: Vec<ResultEntry>) -> Option<LdapUser> {
     let mut entry = SearchEntry::construct(rs.pop()?);
+    println!("Got entry:\n{:?}", entry);
     Some(LdapUser {
-        username: entry.attrs.get_mut("uid")?.pop()?.to_string(),
-        ugkthid: entry.attrs.get_mut("ugkthid")?.pop()?.to_string(),
-        realname: entry.attrs.get_mut("cn")?.pop()?.to_string(),
+        username: entry.attrs.get_mut(LDAP_USERNAME)?.pop()?.to_string(),
+        ugkthid: entry.attrs.get_mut(LDAP_UGKTHID)?.pop()?.to_string(),
+        realname: entry.attrs.get_mut(LDAP_REALNAME)?.pop()?.to_string(),
     })
 }
 
 // ldapsearch -x -H ldaps://ldap.kth.se -b ou=Unix,dc=kth,dc=se uid=ransin ugKthid | grep "ugKthid"
 pub fn fetch_ldap_data(ugkthid: &str) -> Result<LdapUser, Box<dyn std::error::Error>> {
+    println!("fetching ldap data");
     let ldap = LdapConn::new("ldaps://ldap.kth.se")?;
     let (rs, _res) = ldap
         .search(
             "ou=Unix,dc=kth,dc=se",
             Scope::Subtree,
-            &("ugKthid=".to_string() + ugkthid),
-            vec!["ugkthid", "uid", "cn"],
+            &(LDAP_UGKTHID.to_string() + "=" + ugkthid),
+            vec![LDAP_UGKTHID, LDAP_USERNAME, LDAP_REALNAME],
         )?
         .success()?;
     // for entry in rs {
     //     println!("{:?}", SearchEntry::construct(entry));
     // }
+
     convert_to_ldap_user(rs).ok_or(Box::new(LdapError))
 }
 
 // ticket example: ST-675984-sfGECP3JozSUYekz9Vg3-login01
 fn validate_ticket(ticket: &str) -> Option<String> {
+    println!("Validating ticket");
     let url = "https://login.kth.se/serviceValidate?ticket=".to_string()
         + ticket
         + "&service=http://queue.csc.kth.se/auth";
