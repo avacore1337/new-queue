21b5f084ff42aff7334557c2c3f156a1ab012d6f
Anton Bäckström <antonbcm@gmail.com>
2020-05-17 19:55:45 +0200

    Forgot to add new files

---
+++ public/src/viewcomponents/Modals/BroadcastFacultyModal.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import Modal from 'react-bootstrap/Modal';
+import { broadcastFaculty } from '../../actions/assistantActions';
+
+export const ModalType = 'BROADCAST_FACULTY_MODAL';
+
+export default (props: any): JSX.Element => {
+
+  const [message, setMessage] = useState('');
+
+  const dispatch = useDispatch();
+
+  function changeMessage(event: any): void {
+    setMessage(event.target.value);
+  }
+
+  function submitMessage(): void {
+    dispatch(broadcastFaculty(props.queueName, message));
+    props.onHide();
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Broadcast message
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <textarea className="w-100" onChange={changeMessage}>{message}</textarea>
+      </Modal.Body>
+      <Modal.Footer>
+        <button
+          className="btn btn-primary"
+          onClick={submitMessage} >
+            Broadcast
+          </button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
---
+++ public/src/viewcomponents/Modals/SetQueueInformationModal.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux'
+import Modal from 'react-bootstrap/Modal';
+import { setQueueInformation } from '../../actions/assistantActions';
+
+export const ModalType = 'SET_QUEUE_INFORMATION_MODAL';
+
+export default (props: any): JSX.Element => {
+
+  const [message, setMessage] = useState('');
+
+  const dispatch = useDispatch();
+
+  function changeMessage(event: any): void {
+    setMessage(event.target.value);
+  }
+
+  function submitQueueInformation(): void {
+    dispatch(setQueueInformation(props.queueName, message));
+    props.onHide();
+  }
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Set queue information
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <textarea className="w-100" onChange={changeMessage}>{message}</textarea>
+      </Modal.Body>
+      <Modal.Footer>
+        <button
+          className="btn btn-primary"
+          onClick={submitQueueInformation} >
+            Set information
+          </button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
