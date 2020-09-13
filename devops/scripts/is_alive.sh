d266c965704085ec92aeb57a8c3d130262563b3f
Robert Welin-Berger <avacore1337@gmail.com>
2020-09-13 12:02:25 +0200

    updating restarting script

--- devops/scripts/is_alive.sh
+++ devops/scripts/is_alive.sh
@@ -4,13 +4,17 @@ set -eu
 set -o pipefail
 
 restart_system(){
+	# Sleep for 10 more seconds and restart system if it's still down. 
+	# This is to prevent "systemctl restart queue" to trigger this script
+	sleep 10
+	curl localhost/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null && return
 	echo "Restarting queue"
 	/bin/systemctl restart queue
 }
 
 while : 
 do
-	sleep 90
+	sleep 30
 	curl localhost/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null || restart_system
 done
 
