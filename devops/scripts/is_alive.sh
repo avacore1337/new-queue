39adddf26ab1c8fd3f8eecf63f9046dd5e9880ab
Robert Welin-Berger <avacore1337@gmail.com>
2020-10-25 23:37:03 +0100

    configs

--- devops/scripts/is_alive.sh
+++ devops/scripts/is_alive.sh
@@ -7,7 +7,7 @@ restart_system(){
 	# Sleep for 10 more seconds and restart system if it's still down. 
 	# This is to prevent "systemctl restart queue" to trigger this script
 	sleep 10
-	curl localhost/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null && return
+	curl 127.0.0.1:8000/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null && return
 	echo "Restarting queue"
 	/bin/systemctl restart queue
 }
@@ -15,6 +15,6 @@ restart_system(){
 while : 
 do
 	sleep 30
-	curl localhost/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null || restart_system
+	curl 127.0.0.1:8000/api/queues -m 15 2>/dev/null | grep Allmanhandledning > /dev/null || restart_system
 done
 
