546c49c311149789a91966c3974cb3724a48bfab
Robert Welin-Berger <avacore1337@gmail.com>
2021-07-08 19:00:13 +0200

    safety check

--- reset_database.sh
+++ reset_database.sh
@@ -1,1 +1,6 @@
-psql -f init.sql && diesel migration run && psql -f testing.sql
+if [ "$HOSTNAME" = queue ]; then
+    printf '%s\n' "No you don't"
+else
+    psql -f init.sql && diesel migration run && psql -f testing.sql
+fi
+
