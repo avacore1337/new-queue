8f0f6c3ab47976e9aa29dd16e0cab2eed256fa60
Robert Welin-Berger <avacore1337@gmail.com>
2021-07-08 22:14:40 +0200

    correct name

--- src/routes/banners.rs
+++ src/routes/banners.rs
@@ -3,5 +3,5 @@ use rocket_contrib::json::JsonValue;
 
 #[get("/banners")]
 pub fn get_banners(conn: db::DbConn) -> JsonValue {
-    json!({ "queues": db::banners::all(&conn) })
+    json!({ "banners": db::banners::all(&conn) })
 }
